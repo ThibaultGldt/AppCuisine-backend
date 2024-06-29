189248862078387b69058298f79b6db01419f899
Thibault <glandut.thibault@pm.me>
2024-06-29 16:36:54 +0200

    switch gradle

---
+++ src/test/java/com/app/Cuisine/CuisineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Cuisine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuisineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
